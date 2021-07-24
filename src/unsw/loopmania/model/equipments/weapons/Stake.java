4bcf15855d599d216ad671b59bf91b781edf3c36
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2021-07-25 00:15:49 +0800

    change weapon armour shield helmet to interface

--- src/unsw/loopmania/model/equipments/weapons/Stake.java
+++ src/unsw/loopmania/model/equipments/weapons/Stake.java
@@ -1,32 +1,29 @@
 package unsw.loopmania.model.equipments.weapons;
 
 import javafx.beans.property.SimpleIntegerProperty;
+import unsw.loopmania.model.Equipment;
 import unsw.loopmania.model.Weapon;
 
 /**
  * represents an equipped or unequipped sword in the backend world
  */
-public class Stake extends Weapon {
+public class Stake extends Equipment implements Weapon {
 
-    /**
-     * Constructor for Stake
-     */
-    private int attackToVampire = 8;
+    private int attack;
+    private int attackToVampire;
 
     public Stake(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
 
         setAttack(3);
 
-        this.attackToVampire  = 8;
+        this.attackToVampire = 8;
 
         setPrice(150);
 
-        String description = 
-        "===== Stake =====\n"
-        +"+ 3 attack (+ 8 if Vampire)\n";
+        String description = "===== Stake =====\n" + "+ 3 attack (+ 8 if Vampire)\n";
 
-       setDescription(description);
+        setDescription(description);
     }
 
     /**
@@ -34,7 +31,17 @@ public class Stake extends Weapon {
      * @return int 
      */
     public int getAttackToVampire() {
-        return this.attackToVampire ;
+        return this.attackToVampire;
+    }
+
+    @Override
+    public int getAttack() {
+        return this.attack;
+    }
+
+    @Override
+    public void setAttack(int attack) {
+        this.attack = attack;
     }
 
 }
