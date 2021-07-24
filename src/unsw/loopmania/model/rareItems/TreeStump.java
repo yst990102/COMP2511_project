9782c61a8dd3a250e0ff9b5d6190df7a8d8a3f6f
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2021-07-25 00:08:39 +0800

    set anduril as weapon and treestump as shield

--- src/unsw/loopmania/model/rareItems/TreeStump.java
+++ src/unsw/loopmania/model/rareItems/TreeStump.java
@@ -1,15 +1,55 @@
 package unsw.loopmania.model.rareItems;
 
-import javafx.beans.property.SimpleIntegerProperty;
-import unsw.loopmania.model.RareItem;
+import java.net.PortUnreachableException;
 
-public class TreeStump extends RareItem {
+import javafx.beans.property.SimpleIntegerProperty;
+import unsw.loopmania.model.Enemy;
+import unsw.loopmania.model.RareItem;
+import unsw.loopmania.model.Shield;
+import unsw.loopmania.model.enemies.Boss;
+
+public class TreeStump extends RareItem implements Shield {
+    private int defence;
+    private int Boss_damage_multiplier;
+
     public TreeStump(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
 
-        String description = "===== TreeStump =====\n" + "+ 8 defence (+ 12 damage if BOSS)\n";
+        setDefence(8);
+        this.Boss_damage_multiplier = 3;
+
+        String description = "===== TreeStump =====\n" + "+ 8 defence (+ 12 defence if BOSS)\n";
 
         setDescription(description);
         setPrice(500);
     }
+
+    @Override
+    public int getDefence() {
+        return this.defence;
+    }
+
+    public int getDefenceByEnemy(Enemy enemy) {
+        if (enemy instanceof Boss) {
+            return this.defence * Boss_damage_multiplier;
+        } else {
+            return this.defence;
+        }
+    }
+
+    @Override
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    @Override
+    public int getCriticalPercentageDecrease() {
+        return 0;
+    }
+
+    @Override
+    public void setCriticalPercentageDecrease(int critical_percentage_decrease) {
+        return;
+    }
+
 }
