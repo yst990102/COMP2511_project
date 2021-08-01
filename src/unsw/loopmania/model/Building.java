b0debc1d04e0da9708f98d6e8ce1ce67297a42f0
Mengrong Li <z5237133@ad.unsw.edu.au>
2021-08-02 02:55:41 +0800

    update buildings

--- src/unsw/loopmania/model/Building.java
+++ src/unsw/loopmania/model/Building.java
@@ -5,10 +5,15 @@ import org.json.JSONObject;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public class Building extends StaticEntity {
+
     public Building(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x,y);
     }
 
+    /**
+     * save the data to Json
+     * @return JSONObject
+     */
     public JSONObject toJson() {
         JSONObject Info = new JSONObject();
         Info.put("x", this.getX());
